5034dd8030c6e65a37b658e8309b64660e137a00
Gabriel Carmo <gabrielcarmobr14@gmail.com>
2024-06-04 20:53:07 -0300

    TDE 05

---
+++ src/pages/ToDoList.js
@@ -0,0 +1,85 @@
+import { useState } from "react";
+
+import "./ToDoList.css";
+
+const ListItem = ({ toDoItem, toggleCompletion }) => {
+  return (
+    <div
+      className="listItem"
+      style={toDoItem.done ? { backgroundColor: "#b6ebb5" } : null}
+    >
+      <input
+        type="checkbox"
+        checked={toDoItem.done}
+        onChange={() => toggleCompletion(toDoItem.id)}
+        className="inputCheckbox"
+      />
+      {toDoItem.name}
+    </div>
+  );
+};
+
+const ToDoList = ({ pageTitle }) => {
+  const [toDoList, setToDoList] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+
+  const onInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const onClickAddTask = () => {
+    if (!inputValue) return;
+    setToDoList([...toDoList, { name: inputValue, done: false }]);
+    setInputValue("");
+  };
+
+  const onClickRemoveTask = () => {
+    setToDoList(toDoList.filter((item, index) => index !== 0));
+  };
+
+  const toggleCompletion = (task) => {
+    setToDoList(
+      toDoList.map((item) =>
+        item === task ? { ...item, done: !item.done } : item
+      )
+    );
+  };
+
+  return (
+    <div>
+      <div className="header">
+        <h1>{pageTitle}</h1>
+      </div>
+      <div className="inputContainer">
+        <label htmlFor="taskText" className="inputLabel">
+          Digite a sua tarefa
+        </label>
+        <input
+          id="taskText"
+          name="taskText"
+          className="input"
+          value={inputValue}
+          onChange={onInputChange}
+        />
+        <button className="addButton" onClick={onClickAddTask}>
+          +
+        </button>
+        <button className="addButton" onClick={onClickRemoveTask}>
+          -
+        </button>
+      </div>
+      <div className="list">
+        {toDoList.map((toDoItem, index) => (
+          <ListItem
+            key={index}
+            toDoItem={toDoItem}
+            toggleCompletion={toggleCompletion}
+          />
+        ))}
+
+      </div>
+    </div>
+  );
+};
+
+export default ToDoList;
